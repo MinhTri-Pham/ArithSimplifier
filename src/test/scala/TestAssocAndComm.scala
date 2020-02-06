36b8a2ddcff6ea1633aecabff7a7d5d0e7417227
MinhTri-Pham <s1612839@sms.ed.ac.uk>
2020-02-06 23:23:57 +0000

    Added tests for associativity and commutativity (taken from Lift)

---
+++ src/test/scala/TestAssocAndComm.scala
@@ -0,0 +1,48 @@
+import arithmetic._
+import org.junit.Test
+import org.junit.Assert._
+
+class TestAssocAndComm {
+
+  val a: Var = Var("a")
+  val b: Var = Var("b")
+  val c: Var = Var("c")
+  val d: Var = Var("d")
+
+  @Test
+  def testAddition(): Unit = {
+    assertEquals(a + b, b + a)
+    assertEquals(a + b + c, c + b + a)
+
+    assertEquals(a + (b + c), (a + b) + c)
+  }
+
+  @Test
+  def testMultiplication(): Unit = {
+    assertEquals(a * b, b * a)
+    assertEquals(a * b * c, c * b * a)
+
+    assertEquals(a * (b * c), (a * b) * c)
+  }
+
+  @Test
+  def testIntDiv(): Unit = {
+    assertNotEquals(a / b, b /^ a)
+    assertNotEquals(a / b / c, c / b / a)
+
+    assertNotEquals(a / (b / c), (a / b) / c)
+  }
+
+  @Test
+  def testMod(): Unit = {
+    assertNotEquals(a % b, b % a)
+    assertNotEquals(a % b % c, c % b % a)
+
+    assertNotEquals(a % (b % c), (a % b) % c)
+  }
+
+  @Test
+  def EquivalentExpr(): Unit = {
+    assertEquals(a + a + b + c + a + b, 2 * a + a + 2 * b + c)
+  }
+}
